ab24349dbdf81831a3c71eb69f58ee6e07ff210e
zRedShift <ronen.ulanovsky.5@gmail.com>
2019-12-05 10:16:33 +0200

    aoc05, now cleaner

--- aoc05/src/main.rs
+++ aoc05/src/main.rs
@@ -13,61 +13,29 @@ fn parse_input(path: &Path) -> Result<Vec<i32>, Box<dyn Error>> {
         .map_err(|e| e.into())
 }
 
-fn run(numbers: &mut [i32], input: i32) {
+fn run(arr: &mut [i32], input: i32) {
     let mut i = 0;
     loop {
-        let (opcode, p1, p2) = (numbers[i] % 100, numbers[i] / 100 % 10, numbers[i] / 1000);
+        let (opcode, p1, p2) = (arr[i] % 100, arr[i] / 100 % 10, arr[i] / 1000);
         match opcode {
-            99 => break,
-            3 => {
-                numbers[numbers[i + 1] as usize] = input;
-                i += 2;
-            }
-            4 => {
-                println!(
-                    "{}",
-                    if p1 != 0 {
-                        numbers[i + 1]
-                    } else {
-                        numbers[numbers[i + 1] as usize]
-                    }
-                );
-                i += 2;
-            }
+            99 => return,
+            3 => arr[arr[i + 1] as usize] = input,
+            4 => println!("{}", arr[if p1 != 0 { i + 1 } else { arr[i + 1] as usize }]),
             _ => {
-                let val1 = if p1 != 0 {
-                    numbers[i + 1]
-                } else {
-                    numbers[numbers[i + 1] as usize]
-                };
-                let val2 = if p2 != 0 {
-                    numbers[i + 2]
-                } else {
-                    numbers[numbers[i + 2] as usize]
-                };
+                let val1 = arr[if p1 != 0 { i + 1 } else { arr[i + 1] as usize }];
+                let val2 = arr[if p2 != 0 { i + 2 } else { arr[i + 2] as usize }];
                 match opcode {
-                    1 => {
-                        numbers[numbers[i + 3] as usize] = val1 + val2;
-                        i += 4;
-                    }
-                    2 => {
-                        numbers[numbers[i + 3] as usize] = val1 * val2;
-                        i += 4;
-                    }
+                    1 => arr[arr[i + 3] as usize] = val1 + val2,
+                    2 => arr[arr[i + 3] as usize] = val1 * val2,
                     5 => i = if val1 != 0 { val2 as usize } else { i + 3 },
                     6 => i = if val1 == 0 { val2 as usize } else { i + 3 },
-                    7 => {
-                        numbers[numbers[i + 3] as usize] = if val1 < val2 { 1 } else { 0 };
-                        i += 4;
-                    }
-                    8 => {
-                        numbers[numbers[i + 3] as usize] = if val1 == val2 { 1 } else { 0 };
-                        i += 4;
-                    }
+                    7 => arr[arr[i + 3] as usize] = (val1 < val2) as i32,
+                    8 => arr[arr[i + 3] as usize] = (val1 == val2) as i32,
                     _ => unreachable!(),
                 }
             }
         }
+        i += [0, 4, 4, 2, 2, 0, 0, 4, 4][opcode as usize];
     }
 }
 
